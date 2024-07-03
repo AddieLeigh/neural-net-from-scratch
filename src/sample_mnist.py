13c395f9808caf3e139076f6809a98a6dc5aea01
addi <addisonnewcome@gmail.com>
2024-07-03 11:03:48 -0600

    Minimize data and reshape matrices
    Minimized training and testing data for ease of processing. Reduced dimensionality
    of training and testing input matrices. This allows the second dimension to be
    used as a feature vector which is ideal for ANNs as opposed to CNNs. Also
    transposed these matrices for future computations

--- src/sample_mnist.py
+++ src/sample_mnist.py
@@ -1,4 +1,4 @@
-# Setup preliminary imports and test that dataset is correctly imported
+# Import, minimize, and format data before testing to ensure proper implementation
 
 import os
 import gzip
@@ -12,7 +12,17 @@ file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'mnist.pkl.
 with gzip.open(file_path, 'rb') as f:
     data = pickle.load(f, encoding= "latin1")
 
-(x_train, y_train), (x_test, y_test) = data
+(X_train, Y_train), (X_test, Y_test) = data
+
+# Seperate out only first 1000 training examples
+x_train = X_train[0:1000]
+y_train = Y_train[0:1000]
+x_test = X_test[0:100]
+y_test = Y_test[0:100]
+
+# Reshape into 2d matrix
+x_train = x_train.reshape((1000, 784,)).T
+x_test = x_test.reshape((100, 784,)).T
 
 # Print test statements to confirm shape of each matrix
 print(f"x_train shape: {x_train.shape}")
